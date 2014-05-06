c638818e65ea3d5a0f34bd6b6946195783e17cb5
Guillaume LE VAILLANT <guillaume.le.vaillant@openmailbox.org>
2014-05-06 14:58:31 +0200

    Add dutch translation.

--- README.md
+++ README.md
@@ -41,3 +41,7 @@ Other available functions in the peercoin-calculator package:
     (pos-prob-days coins days difficulty n)
     (pow-reward difficulty)
     (pow-prob-time difficulty rate time)
+
+## Donation
+
+If you find this program useful and want to make a donation, you can send coins to the following Peercoin address: *PWFNV1Cvq7nQBRyRueuYzwmDNXUGpgNkBC*.
---
+++ translations/nl.ts
@@ -0,0 +1,86 @@
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>peercoin-calculator</name>
+    <message>
+      <source>Calculate</source>
+      <translation>Berekenen</translation>
+    </message>
+    <message>
+      <source>Proof-Of-Stake (POS)</source>
+      <translation>Bewijs-van-Aandeel (POS)</translation>
+    </message>
+    <message>
+      <source>Proof-Of-Work (POW)</source>
+      <translation>Bewijs-van-Werk (POW)</translation>
+    </message>
+    <message>
+      <source>10 min</source>
+      <translation>10 min</translation>
+    </message>
+    <message>
+      <source>24 hours</source>
+      <translation>24 uur</translation>
+    </message>
+    <message>
+      <source>31 days</source>
+      <translation>31 dagen</translation>
+    </message>
+    <message>
+      <source>90 days</source>
+      <translation>90 dagen</translation>
+    </message>
+    <message>
+      <source>1 year</source>
+      <translation>1 jaar</translation>
+    </message>
+    <message>
+      <source>Probability of POS block</source>
+      <translation>Waarschijnlijkheid van POS blok</translation>
+    </message>
+    <message>
+      <source>Probability of POW block</source>
+      <translation>Waarschijnlijkheid van POW blok</translation>
+    </message>
+    <message>
+      <source>Reward</source>
+      <translation>Beloning</translation>
+    </message>
+    <message>
+      <source>Total</source>
+      <translation>Totaal</translation>
+    </message>
+    <message>
+      <source>POS block</source>
+      <translation>POS blok</translation>
+    </message>
+    <message>
+      <source>POW block</source>
+      <translation>POW blok</translation>
+    </message>
+    <message>
+      <source>Peercoin Calculator</source>
+      <translation>Peercoin Calculator</translation>
+    </message>
+    <message>
+      <source>Number of coins</source>
+      <translation>Aantal munten</translation>
+    </message>
+    <message>
+      <source>Age of coins (days)</source>
+      <translation>Ouderdom van munten (dagen)</translation>
+    </message>
+    <message>
+      <source>POS difficulty</source>
+      <translation>POS moeilijkheid</translation>
+    </message>
+    <message>
+      <source>Hash rate</source>
+      <translation>Hash snelheid</translation>
+    </message>
+    <message>
+      <source>POW difficulty</source>
+      <translation>POW moeilijkheid</translation>
+    </message>
+  </context>
+</TS>
